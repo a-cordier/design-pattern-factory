558fac16dc31cd4437b95186034c8425d4c4d18f
acordier <anton.36ve@gmail.com>
2014-09-08 22:53:20 +0200

    cosmetics

--- src/com/acordier/patterns/Main.java
+++ src/com/acordier/patterns/Main.java
@@ -3,8 +3,8 @@ package com.acordier.patterns;
 public class Main {
 
 	public static void main(String[] args) {
-
-
+		Service service = ServiceFactory.newService(MessageService.class);
+		System.out.println(service.toString());
 	}
 
 }
--- src/com/acordier/patterns/ServiceFactory.java
+++ src/com/acordier/patterns/ServiceFactory.java
@@ -2,11 +2,11 @@ package com.acordier.patterns;
 
 public class ServiceFactory {
 	
-	public static Service newService(Class<? extends Service> clazz, String name, String description){
+	public static Service newService(Class<? extends Service> clazz){
 		if(clazz.equals(MessageService.class)){
-			return new MessageService(name, description);
+			return new MessageService("Message service", "Use this service to send and receive messages");
 		}else if(clazz.equals(EventService.class)){
-			return new EventService(name, description);
+			return new EventService("Event service", "Use this service to fire and catch events");
 		}
 		throw new ServiceFactoryException("Unknown concrete type ");
 	}
